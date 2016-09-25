9852b2f825c85bf2d4d9a366b08c02a3e0037fc9
julia-castro <julia.castro@outlook.com>
2016-09-25 18:00:18 -0400

    Initial Commit

---
+++ StrutsProblem/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StrutsProblem
+//
+//  Created by Julia Castro on 9/25/16.
+//  Copyright © 2016 Julia Castro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
